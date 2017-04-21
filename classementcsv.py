33ee7b411e821341fa98ffa489e5deb417d4805d
Thomas <thomas.cornillet@gmail.com>
2017-04-21 19:37:46 +0200

    Première version qui fonctionne

--- classementcsv.py
+++ classementcsv.py
@@ -1,5 +1,45 @@
 #!/usr/bin/python3
 # -*-coding:Utf-8 -*
 
+"""
+
+Ça fonctionne, mais pour il y a un problème : vu que le csv est créé avant le parcours du dossier, le fichier csv est pris en compte dans l'énumération
+
+"""
+
 import os
 import csv
+
+# on se déplace dans le dossier cible (évolution possible, lancer le script directement en passant l'adresse du dossier cible en argument
+rep = 'n'
+while rep == 'n':
+	path = input("Quelle est l'adresse du dossier cible ?\n")
+	os.chdir(path)
+	retval = os.getcwd()
+	print("Le dossier courant est {}".format(retval))
+	rep = input("Est-ce correct ? o/n\n")
+
+# on crée une liste avec les fichiers du dossier cible
+fichiers = os.listdir(path)
+nb_fichiers = len(fichiers)
+
+# on crée le csv et son en-tête
+fw = open('classement.csv', 'w') # ne pas oublier de fermer avec f.close() lors des tests
+
+entetes = ["nom", "date", "owner", "extension"]
+ligneEntete = ",".join(entetes) + "\n"
+fw.write(ligneEntete)
+
+
+i = 1
+while i < nb_fichiers:
+	valeurs = fichiers[i]
+	valeur = valeurs.split(".")
+	info = valeur[0]
+	extension = valeur[1]
+	info = info.split(" - ")
+	ligne = ",".join(info) + "," + extension + "\n"
+	fw.write(ligne)
+	i += 1
+
+fw.close()
